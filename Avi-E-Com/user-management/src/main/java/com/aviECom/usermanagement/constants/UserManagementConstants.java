141c733751163b7b09baa51ac18f5e1e8136d183
Avinash831999 <155517953+Avinash831999@users.noreply.github.com>
2024-02-15 15:19:47 +0530

    Delete Avi-E-Com/user-management directory

--- Avi-E-Com/user-management/src/main/java/com/aviECom/usermanagement/constants/UserManagementConstants.java
+++
@@ -1,9 +0,0 @@
-package com.aviECom.usermanagement.constants;
-
-public final class UserManagementConstants {
-	
-	public static final String userCreated = "1111 User added";
-	public static final String userDeleted = "0000 User deleted";
-	public static final String userUpdated = "1010 User updated";
-	
-}
