8e874e518bf6a4dade516d4227c218ae7a423eb0
Avinash831999 <155517953+Avinash831999@users.noreply.github.com>
2024-02-15 15:16:12 +0530

    Delete Avi-E-Com/order-management

--- Avi-E-Com/order-management/src/main/java/com/aviECom/ordermanagement/controllers/OrderController.java
+++
@@ -1,9 +0,0 @@
-package com.aviECom.ordermanagement.controllers;
-
-import com.aviECom.ordermanagement.dtos.OrderRequestDto;
-
-public interface OrderController {
-	
-	public OrderRequestDto placeOrder(OrderRequestDto orderReq);
-	
-}
