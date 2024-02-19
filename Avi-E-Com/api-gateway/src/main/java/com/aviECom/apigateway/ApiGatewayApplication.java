f4b92eaf5447bd98d65595b902fc1c13cc42827e
Avinash <avinashaswin250@gmail.com>
2024-02-19 20:30:07 +0530

    API Gateway added

--- Avi-E-Com/api-gateway/src/main/java/com/aviECom/apigateway/ApiGatewayApplication.java
+++ Avi-E-Com/api-gateway/src/main/java/com/aviECom/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,25 @@ package com.aviECom.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+    public RouteLocator getRoute(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/users/**").filters(f -> f.stripPrefix(1)).uri("lb://USER-MANAGEMENT"))
+				.route(p -> p.path("/orders/**").filters(f -> f.stripPrefix(1)).uri("lb://ORDER-MANAGEMENT"))
+				.route(p -> p.path("/users/**").filters(f -> f.stripPrefix(1)).uri("lb://USER-MANAGEMENT"))
+				.route(p -> p.path("/products/**").filters(f -> f.stripPrefix(1)).uri("lb://PRODUCT-MANAGEMENT"))
+				.build() ;
 	}
 
 }
